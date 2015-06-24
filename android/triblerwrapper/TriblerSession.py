562329287a56e498b876f10742fe58e65207f52f
GitlabUser1234 <m8r-guo7r21@mailinator.com>
2015-06-24 11:39:38 +0200

    Cleanup config edita

--- android/triblerwrapper/TriblerSession.py
+++ android/triblerwrapper/TriblerSession.py
@@ -89,15 +89,13 @@ class TriblerSession(BaseManager):
         self._sconfig.set_state_dir(os.environ['TRIBLER_STATE_DIR'])
 
         # Disable unwanted dependencies:
-        self._sconfig.set_torrent_store(False)
+        # Disable because feedparser is not available
         self._sconfig.set_enable_channel_search(False)
-        self._sconfig.set_videoplayer(False)  # Now what?
-        self._sconfig.set_preferred_playback_mode(42)  # Now what? 2
-        self._sconfig.set_torrent_checking(True)
-        self._sconfig.set_multicast_local_peer_discovery(False)
-        self._sconfig.set_mainline_dht(True)
-        self._sconfig.set_dht_torrent_collecting(True)
-        self._sconfig.set_torrent_collecting_max_torrents(5000)
+
+        # Videoplayer stuff
+        self._sconfig.set_videoplayer(False)
+        self._sconfig.set_preferred_playback_mode(42)
+
 
         Logger.info("Starting Tribler session..")
         self._session = Session(self._sconfig)
